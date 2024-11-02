e1b27ce7f979e41897bfc84dca398fe5c71bf354
William Cotton <williamcotton@gmail.com>
2024-11-01 22:42:56 -0500

    monaco editor as search input

--- react-search-component/src/App.tsx
+++ react-search-component/src/App.tsx
@@ -1,13 +1,17 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Editor, { OnMount } from "@monaco-editor/react";
 import type { editor } from "monaco-editor";
 import "./App.css";
 
 function App() {
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const [monaco, setMonaco] = useState<typeof import("monaco-editor") | null>(null)
+  const [rangeOffset, setRangeOffset] = useState(1);
+  const [value, setValue] = useState<string | undefined>("// some comment");
 
   const handleEditorDidMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
+    setMonaco(monaco);
     editor.createDecorationsCollection([
       {
         range: new monaco.Range(1, 4, 1, 10),
@@ -15,33 +19,83 @@ function App() {
       },
     ]);
 
+    editor.addAction({
+      id: "submitInSingleMode",
+      label: "Submit in single mode",
+      // Monaco ships with out of the box enums for keycodes and modifiers
+      keybindings: [monaco.KeyCode.Enter],
+      run: () => {
+        console.log("123")
+      },
+    });
   };
+
+  const onChange = (value: string | undefined) => {
+    setValue(value);
+    const editor = editorRef.current;
+    setRangeOffset(rangeOffset + 1);
+    return editor && monaco ? editor.createDecorationsCollection([
+      {
+        range: new monaco.Range(1, 4 + rangeOffset, 1, 10 + rangeOffset),
+        options: { inlineClassName: "myInlineDecoration" },
+      },
+    ]) : null
+  }
 
   return (
     <>
+      <p>test</p>
       <Editor
-        height="1.5em"
+        height="2.1em"
         width="90vh"
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
+        defaultLanguage=""
+        defaultValue={value}
         onMount={handleEditorDidMount}
+        className="search-input"
+        onChange={onChange}
         options={{
-          minimap: {
-            enabled: false,
-            showSlider: "mouseover",
-          },
-          scrollBeyondLastLine: false,
-          fontSize: 14,
-          automaticLayout: true,
-          lineNumbers: "off",
-          glyphMargin: false,
-          folding: false,
           renderLineHighlight: "none",
+          quickSuggestions: false,
+          glyphMargin: false,
           lineDecorationsWidth: 0,
+          folding: false,
+          fixedOverflowWidgets: true,
+          acceptSuggestionOnEnter: "on",
+          hover: {
+            delay: 100,
+          },
+          roundedSelection: false,
+          contextmenu: false,
+          cursorStyle: "line-thin",
+          occurrencesHighlight: "off",
+          links: false,
+          minimap: { enabled: false },
+          // see: https://github.com/microsoft/monaco-editor/issues/1746
+          wordBasedSuggestions: "off",
+          // disable `Find`
+          find: {
+            addExtraSpaceOnTop: false,
+            autoFindInSelection: "never",
+            seedSearchStringFromSelection: "never",
+          },
+          fontSize: 14,
+          fontWeight: "normal",
+          wordWrap: "off",
+          lineNumbers: "off",
           lineNumbersMinChars: 0,
           overviewRulerLanes: 0,
+          overviewRulerBorder: false,
+          hideCursorInOverviewRuler: true,
+          scrollBeyondLastColumn: 0,
+          scrollbar: {
+            horizontal: "hidden",
+            vertical: "hidden",
+            // avoid can not scroll page when hover monaco
+            alwaysConsumeMouseWheel: false,
+          },
         }}
       />
+      <p>test</p>
     </>
   );
 }
